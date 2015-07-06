d2b53d8d95fd8b8b57ae3f9274cfbcb9d492ee96
Alexander Hoertenhuber <alex.hoertenhuber@gmx.at>
2015-07-06 16:07:11 +0200

    First commit

---
+++ src/chessboard.cpp
@@ -0,0 +1,31 @@
+#include <QApplication>
+#include <QtWidgets>
+#include <QLabel>
+#include <QGridLayout>
+#include <QGroupBox>
+#include <QVector>
+#include <QString>
+#include <fen.h>
+#include <board.h>
+#include <iostream>
+#include <mywindow.h>
+
+#include <chessdatabase.h>
+#include <vector>
+#include <string>
+
+#include <boost/thread.hpp>
+#include<fstream>
+#include<cstdlib>
+#include<sstream>
+#include <sys/types.h>  // mkfifo
+#include <sys/stat.h>   // mkfifo
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    QApplication app(argc, argv);
+    MyWindow window;
+    window.show();
+    return app.exec();
+}
